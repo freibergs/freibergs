303d8cd578792845ad5574ac5dd890c28ce2967c
zajebs <hello@rihards.dev>
2023-09-12 12:04:15 +0300

    Dynamic portfolio JSON

---
+++ assets/js/wordpress.js
@@ -0,0 +1,22 @@
+fetch('assets/data/projects.json')
+    .then(response => response.json())
+    .then(projects => {
+        const container = document.getElementById('projectsContainer');
+        
+        projects.forEach(project => {
+            const fullLink = `https://${project.title}`;
+            const updatedDescription = project.description.replace(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/gi, function(match) {
+                return `<a href="https://${match}">${match}</a>`;
+            });
+
+            const projectHTML = `
+                <h3>${project.title}</h3>
+                <span class="image main"><img src="${project.image}" alt="" /></span>
+                <p><a href="${fullLink}">${project.title}</a> ${updatedDescription}</p>
+            `;
+            container.innerHTML += projectHTML;
+        });
+    })
+    .catch(error => {
+        console.error("Kļūda ielādējot projektus:", error);
+    });
