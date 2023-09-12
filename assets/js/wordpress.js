2e3f91a234139eebe848ffefb0f8950bc22aedd8
zajebs <hello@rihards.dev>
2023-09-12 12:16:27 +0300

    Update wordpress.js

--- assets/js/wordpress.js
+++ assets/js/wordpress.js
@@ -4,18 +4,18 @@ fetch('assets/data/projects.json')
         const container = document.getElementById('projectsContainer');
         
         projects.forEach(project => {
-            const fullLink = `https://${project.title}`;
-            const updatedDescription = project.description.replace(/(([\da-z\.-]+)\.(lv|com))/gi, function(match) {
+			const updatedDescription = project.description.replace(/(([\da-z\.-]+)\.(lv|com))/gi, function(match) {
 				return `<a href="https://${match}">${match}</a>`;
 			});
 
-            const projectHTML = `
-                <h3>${project.title}</h3>
-                <span class="image main"><img src="${project.image}" alt="" /></span>
-                <p><a href="${fullLink}">${project.title}</a> ${updatedDescription}</p>
-            `;
-            container.innerHTML += projectHTML;
-        });
+			const projectHTML = `
+				<h3>${project.title}</h3>
+				<span class="image main"><img src="${project.image}" alt="" /></span>
+				<p>${updatedDescription}</p>
+			`;
+
+			container.innerHTML += projectHTML;
+		});
     })
     .catch(error => {
         console.error("Kļūda ielādējot projektus:", error);
